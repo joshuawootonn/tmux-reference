4c8cd9e1c294cce3967452efa936e3677556e30e
Joshua Wootonn <joshuawootonn@gmail.com>
2025-04-15 08:03:04 -0400

    .

--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from "react";
 export function Keyboard({ children }: { children: ReactNode }) {
   return (
     <>
-      <kbd className="mr-2 !ml-5.5 inline-block">ctrl + b</kbd>,
+      <kbd className="mr-2 !ml-3.5 inline-block">ctrl + b</kbd>,
       <kbd className="ml-1 inline-block">{children}</kbd>{" "}
     </>
   );
