5c099c587edbf14f48f76851a6ce071d6e563ad9
Joshua Wootonn <joshuawootonn@gmail.com>
2025-04-15 08:14:45 -0400

    .

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,11 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring focus-visible:ring-[2px]",
+  cn(
+    "inline-flex items-center shrink-0 justify-center gap-2 whitespace-nowrap text-sm font-medium",
+    "disabled:pointer-events-none disabled:opacity-50",
+    "[&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0",
+  ),
   {
     variants: {
       variant: {
