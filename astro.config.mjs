08061d4a7401c269514d884636dfdd3593c9f047
Joshua Wootonn <joshuawootonn@gmail.com>
2025-04-20 09:34:03 -0400

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,15 @@ import mdx from "@astrojs/mdx";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [mdx(), react()],
+  site: "https://www.tmuxreference.com",
+  integrations: [mdx(), react(), sitemap()],
   env: {
     schema: {
       FATHOM_ID: envField.string({
